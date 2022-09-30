27e6fb0085e03715b30a72e766561f0ea8c248e4
jakubmakowka <83394180+jakubmakowka@users.noreply.github.com>
2022-09-30 17:01:55 +0200

    minor change cleanup
    removed .net 5.0 files

--- TrikProjekt/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrikProjekt56.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
